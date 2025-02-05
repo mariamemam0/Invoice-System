9821ade33d2b4f397b6e069d66270cec02961348
mariam <mariamemampp00@gmail.com>
2025-02-05 16:24:10 +0200

    add css , js links for invoices view

--- resources/views/invoices/invoices.blade.php
+++ resources/views/invoices/invoices.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.master')
+@section('title')
+قائمة الفواتير
+@stop
 @section('css')
+<!-- Internal Data table css -->
+<link href="{{URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
 @endsection
 @section('page-header')
 				<!-- breadcrumb -->
@@ -9,38 +19,85 @@
 							<h4 class="content-title mb-0 my-auto">الفواتير</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0"> /قائمةالفواتير </span>
 						</div>
 					</div>
-					<div class="d-flex my-xl-auto right-content">
-						<div class="pr-1 mb-3 mb-xl-0">
-							<button type="button" class="btn btn-info btn-icon ml-2"><i class="mdi mdi-filter-variant"></i></button>
-						</div>
-						<div class="pr-1 mb-3 mb-xl-0">
-							<button type="button" class="btn btn-danger btn-icon ml-2"><i class="mdi mdi-star"></i></button>
-						</div>
-						<div class="pr-1 mb-3 mb-xl-0">
-							<button type="button" class="btn btn-warning  btn-icon ml-2"><i class="mdi mdi-refresh"></i></button>
-						</div>
-						<div class="mb-3 mb-xl-0">
-							<div class="btn-group dropdown">
-								<button type="button" class="btn btn-primary">14 Aug 2019</button>
-								<button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" id="dropdownMenuDate" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<span class="sr-only">Toggle Dropdown</span>
-								</button>
-								<div class="dropdown-menu dropdown-menu-left" aria-labelledby="dropdownMenuDate" data-x-placement="bottom-end">
-									<a class="dropdown-item" href="#">2015</a>
-									<a class="dropdown-item" href="#">2016</a>
-									<a class="dropdown-item" href="#">2017</a>
-									<a class="dropdown-item" href="#">2018</a>
-								</div>
-							</div>
-						</div>
-					</div>
+					
 				</div>
 				<!-- breadcrumb -->
 @endsection
 @section('content')
 				<!-- row -->
 				<div class="row">
+				
+					
+					
+					<div class="col-xl-12">
+						<div class="card">
+							<div class="card-header pb-0">
+								<div class="d-flex justify-content-between">
+									<h4 class="card-title mg-b-0">STRIPED ROWS</h4>
+									<i class="mdi mdi-dots-horizontal text-gray"></i>
+								</div>
+								<p class="tx-12 tx-gray-500 mb-2">Example of Valex Striped Rows.. <a href="">Learn more</a></p>
+							</div>
+							<div class="card-body">
+								<div class="table-responsive">
+									<table class="table text-md-nowrap" id="example2">
+										<thead>
+											<tr>
+												<th class="wd-15p border-bottom-0"> #</th>
+												<th class="wd-15p border-bottom-0">رقم الفاتورةه </th>
+												<th class="wd-20p border-bottom-0">تاريخ الفاتورة</th>
+												<th class="wd-15p border-bottom-0"> تاريخ الإستحقاق</th>
+												<th class="wd-10p border-bottom-0">المنتج</th>
+												<th class="wd-25p border-bottom-0">القسم</th>
+												<th class="wd-25p border-bottom-0">تاريخ الإستحقاق</th>
+												<th class="wd-25p border-bottom-0">الخصم</th>
+												<th class="wd-25p border-bottom-0">نسبة الضريبة</th>
+												<th class="wd-25p border-bottom-0">قيمة الضريبة</th>
+												<th class="wd-25p border-bottom-0">الإجمالي</th>
+												<th class="wd-25p border-bottom-0">الحالة</th>
+												<th class="wd-25p border-bottom-0">الملاحظات</th>
 
+
+
+
+
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>1</td>
+												<td>41212</td>
+												<td>System Developer</td>
+												<td>2018/03/12</td>
+												<td>$654,765</td>
+												<td>b.Chloe@datatables.net</td>
+												<td>Bella</td>
+												<td>Chloe</td>
+												<td>System Developer</td>
+												<td>2018/03/12</td>
+												<td>$654,765</td>
+												<td>b.Chloe@datatables.net</td>
+												<td>b.Chloe@datatables.net</td>
+											</tr>
+										
+											
+										</tbody>
+									</table>
+								</div>
+							</div><!-- bd -->
+						</div><!-- bd -->
+					</div>
+					<!--/div-->
+
+					<!--div-->
+					  
+					<!--/div-->
+
+					<!--div-->
+					<div class="col-xl-12">
+						
+					</div>
+				
 				</div>
 				<!-- row closed -->
 			</div>
@@ -49,4 +106,23 @@
 		<!-- main-content closed -->
 @endsection
 @section('js')
+<!-- Internal Data tables -->
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+<!--Internal  Datatable js -->
+<script src="{{URL::asset('assets/js/table-data.js')}}"></script>
 @endsection
