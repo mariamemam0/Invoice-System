d7a64cf2694b2d458c6ba51e912c712e00d2bca5
mariam <mariamemampp00@gmail.com>
2025-02-15 20:41:15 +0200

    make route for product and section

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\InvoicesController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SectionsController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('invoices',InvoicesController::class);
 Route::resource('sections',SectionsController::class);
+Route::resource('products',ProductController::class);
 
 Route::get('/{page}', [AdminController::class,'index']);
 
