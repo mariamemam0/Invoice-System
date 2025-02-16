c88003ab8fd451daa2912afafb63961fac57eaa8
mariam <mariamemampp00@gmail.com>
2025-02-16 15:46:08 +0200

    add data from data base and add edit buton

--- resources/views/products/products.blade.php
+++ resources/views/products/products.blade.php
@@ -1,147 +1,236 @@
 @extends('layouts.master')
 @section('css')
-<!-- Internal Data table css -->
-<link href="{{URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
-<link href="{{URL::asset('assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
-<link href="{{URL::asset('assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
-<link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
-<link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
-<link href="{{URL::asset('assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
+	<!-- Internal Data table css -->
+	<link href="{{URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+	<link href="{{URL::asset('assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+	<link href="{{URL::asset('assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+	<link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+	<link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+	<link href="{{URL::asset('assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
 @endsection
 @section('title')
-    المنتجات
+المنتجات
 @stop
 @section('page-header')
-				<!-- breadcrumb -->
-				<div class="breadcrumb-header justify-content-between">
-					<div class="my-auto">
-						<div class="d-flex">
-							<h4 class="content-title mb-0 my-auto">الإعدادات</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ المنتجات</span>
-						</div>
-					</div>
-		
-				</div>
-				<!-- breadcrumb -->
+	<!-- breadcrumb -->
+	<div class="breadcrumb-header justify-content-between">
+		<div class="my-auto">
+			<div class="d-flex">
+				<h4 class="content-title mb-0 my-auto">الإعدادات</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/
+					المنتجات</span>
+			</div>
+		</div>
+
+	</div>
+	<!-- breadcrumb -->
 @endsection
 @section('content')
-@if (session()->has('Add'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <strong>{{ session()->get('Add') }}</strong>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-    @endif
-
-				<!-- row -->
-				<div class="row">
-				<div class="col-xl-12">
-						<div class="card">
-							<div class="card-header pb-0">
-							<div class="col-sm-6 col-md-3">
-							<button type="button" class="btn btn-primary-gradient btn-block" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-plus"></i>&nbsp; اضافة منتج</button><br><br>
-								</div>
-							</div>
-							<div class="card-body">
-								<div class="table-responsive">
-									<table class="table text-md-nowrap" id="example1"  data-page-length="50">
-										<thead>
-											<tr>
-												<th class="wd-15p border-bottom-0"> #</th>
-												<th class="wd-15p border-bottom-0">إسم المنتج </th>
-												<th class="wd-20p border-bottom-0">إسم القسم</th>
-												<th class="wd-15p border-bottom-0"> ملاحظات</th>
-												<th class="wd-10p border-bottom-0">العمليات</th>
-												
-
-
-
-
-
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<td>1</td>
-												<td>41212</td>
-												<td>System Developer</td>
-												<td>2018/03/12</td>
-												<td>$654,765</td>
-												
-											</tr>
-										
-											
-										</tbody>
-									</table>
-								</div>
-							</div><!-- bd -->
-						</div><!-- bd -->
-					</div>
-					<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">اضافة منتج</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                    <form action="{{route('products.store') }}" method="post">
-                                    {{csrf_field()}}
-                                    <div class="modal-body">
-                                        <div class="form-group">
-                                            <label for="exampleInputEmail1">اسم المنتج</label>
-                                            <input type="text" class="form-control" id="product_name" name="product_name" required >
-
-                                        </div>
-
-                                        <label class="my-1 mr-2" for="inlineFormCustomSelectPref">القسم</label>
-                                        <select name="section_id" id="section_id" class="form-control" required>
-                                            <option value="" selected disabled> --حدد القسم--</option>
-                                            @foreach ($sections as $section)
-                                                <option value="{{ $section->id }}">{{ $section->section_name }}</option>
-                                            @endforeach
-                                        </select>
-
-                                        <div class="form-group">
-                                            <label for="exampleFormControlTextarea1">ملاحظات</label>
-                                            <textarea class="form-control" id="description" name="description" rows="3"></textarea>
-                                        </div>
-
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="submit" class="btn btn-success">تاكيد</button>
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">اغلاق</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-				</div>
-				<!-- row closed -->
-			</div>
-			<!-- Container closed -->
+	@if (session()->has('Add'))
+		<div class="alert alert-success alert-dismissible fade show" role="alert">
+			<strong>{{ session()->get('Add') }}</strong>
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
 		</div>
-		<!-- main-content closed -->
+	@endif
+
+	<!-- row -->
+	<div class="row">
+		<div class="col-xl-12">
+			<div class="card">
+				<div class="card-header pb-0">
+					<div class="col-sm-6 col-md-3">
+						<button type="button" class="btn btn-primary-gradient btn-block" data-toggle="modal"
+							data-target="#exampleModal"><i class="fas fa-plus"></i>&nbsp; اضافة منتج</button><br><br>
+					</div>
+				</div>
+				<div class="card-body">
+					<div class="table-responsive">
+						<table class="table text-md-nowrap" id="example1" data-page-length="50">
+							<thead>
+								<tr>
+									<th class="wd-15p border-bottom-0"> #</th>
+									<th class="wd-15p border-bottom-0">إسم المنتج </th>
+									<th class="wd-20p border-bottom-0">إسم القسم</th>
+									<th class="wd-15p border-bottom-0"> ملاحظات</th>
+									<th class="wd-10p border-bottom-0">العمليات</th>
+
+
+
+
+
+
+								</tr>
+							</thead>
+							<tbody>
+                            <?php  $i = 0 ?>
+								@foreach($products as $product)
+								<?php  $i++?>
+									<tr>
+										<td>{{ $i }}</td>
+										<td>{{ $product->product_name }}</td>
+										<td>{{ $product->section->section_name}}</td>
+										<td>{{ $product->description }}</td>
+										<td>
+                                                <button class="btn btn-outline-success btn-sm"
+                                                    data-name="{{ $product->product_name }}" data-pro_id="{{ $product->id }}"
+                                                    data-section_name="{{ $product->section->section_name }}"
+                                                    data-description="{{ $product->description }}" data-toggle="modal"
+                                                    data-target="#edit_Product">تعديل</button>
+                                            
+
+                                                <button class="btn btn-outline-danger btn-sm " data-pro_id="{{ $product->id }}"
+                                                    data-product_name="{{ $product->product_name }}" data-toggle="modal"
+                                                    data-target="#modaldemo9">حذف</button>
+
+                                        </td>
+
+									</tr>
+								@endforeach
+
+							</tbody>
+						</table>
+					</div>
+				</div><!-- bd -->
+			</div><!-- bd -->
+		</div>
+		<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+			aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">اضافة منتج</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<form action="{{route('products.store') }}" method="post">
+						{{csrf_field()}}
+						<div class="modal-body">
+							<div class="form-group">
+								<label for="exampleInputEmail1">اسم المنتج</label>
+								<input type="text" class="form-control" id="product_name" name="product_name" required>
+
+							</div>
+
+							<label class="my-1 mr-2" for="inlineFormCustomSelectPref">القسم</label>
+							<select name="section_id" id="section_id" class="form-control" required>
+								<option value="" selected disabled> --حدد القسم--</option>
+								@foreach ($sections as $section)
+									<option value="{{ $section->id }}">{{ $section->section_name }}</option>
+								@endforeach
+							</select>
+
+							<div class="form-group">
+								<label for="exampleFormControlTextarea1">ملاحظات</label>
+								<textarea class="form-control" id="description" name="description" rows="3"></textarea>
+							</div>
+
+						</div>
+						<div class="modal-footer">
+							<button type="submit" class="btn btn-success">تاكيد</button>
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">اغلاق</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		<div class="modal fade" id="edit_Product" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">تعديل منتج</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form action='products/update' method="post">
+                        {{ method_field('patch') }}
+                        {{ csrf_field() }}
+                        <div class="modal-body">
+
+                            <div class="form-group">
+                                <label for="title">اسم المنتج :</label>
+
+                                <input type="hidden" class="form-control" name="pro_id" id="pro_id" value="">
+
+                                <input type="text" class="form-control" name="Product_name" id="Product_name">
+                            </div>
+
+                            <label class="my-1 mr-2" for="inlineFormCustomSelectPref">القسم</label>
+                            <select name="section_name" id="section_name" class="custom-select my-1 mr-sm-2" required>
+                                @foreach ($sections as $section)
+                                    <option>{{ $section->section_name }}</option>
+                                @endforeach
+                            </select>
+
+                            <div class="form-group">
+                                <label for="des">ملاحظات :</label>
+                                <textarea name="description" cols="20" rows="5" id='description'
+                                    class="form-control"></textarea>
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">تعديل البيانات</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">اغلاق</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+	</div>
+	<!-- row closed -->
+	</div>
+	<!-- Container closed -->
+	</div>
+	<!-- main-content closed -->
 @endsection
 @section('js')
-<!-- Internal Data tables -->
-<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
-<!--Internal  Datatable js -->
-<script src="{{URL::asset('assets/js/table-data.js')}}"></script>
+	<!-- Internal Data tables -->
+	<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+	<script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+	<!--Internal  Datatable js -->
+	<script src="{{URL::asset('assets/js/table-data.js')}}"></script>
+	<script>
+        $('#edit_Product').on('show.bs.modal', function(event) {
+            var button = $(event.relatedTarget)
+            var Product_name = button.data('name')
+            var section_name = button.data('section_name')
+            var pro_id = button.data('pro_id')
+            var description = button.data('description')
+            var modal = $(this)
+            modal.find('.modal-body #Product_name').val(Product_name);
+            modal.find('.modal-body #section_name').val(section_name);
+            modal.find('.modal-body #description').val(description);
+            modal.find('.modal-body #pro_id').val(pro_id);
+        })
+
+
+        $('#modaldemo9').on('show.bs.modal', function(event) {
+            var button = $(event.relatedTarget)
+            var pro_id = button.data('pro_id')
+            var product_name = button.data('product_name')
+            var modal = $(this)
+
+            modal.find('.modal-body #pro_id').val(pro_id);
+            modal.find('.modal-body #product_name').val(product_name);
+        })
+
+    </script>
 @endsection
