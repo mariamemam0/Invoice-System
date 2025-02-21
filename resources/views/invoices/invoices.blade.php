b90d836c5007419e81c8f559fb42647ed419c624
mariam <mariamemampp00@gmail.com>
2025-02-21 12:40:56 +0200

    improve view to add invoices

--- resources/views/invoices/invoices.blade.php
+++ resources/views/invoices/invoices.blade.php
@@ -29,14 +29,15 @@
 				
 					
 					
-					<div class="col-xl-12">
-						<div class="card">
-							<div class="card-header pb-0">
-								<div class="d-flex justify-content-between">
-									<h4 class="card-title mg-b-0">STRIPED ROWS</h4>
-									<i class="mdi mdi-dots-horizontal text-gray"></i>
-								</div>
-								<p class="tx-12 tx-gray-500 mb-2">Example of Valex Striped Rows.. <a href="">Learn more</a></p>
+				<div class="col-xl-12">
+            <div class="card mg-b-20">
+                <div class="card-header pb-0">
+                        <a href="invoices/create" class="modal-effect btn btn-sm btn-primary" style="color:white"><i
+                                class="fas fa-plus"></i>&nbsp; اضافة فاتورة</a>
+                    
+
+                        <a class="modal-effect btn btn-sm btn-primary" href="{{ url('export_invoices') }}"
+                            style="color:white"><i class="fas fa-file-download"></i>&nbsp;تصدير اكسيل</a>
 							</div>
 							<div class="card-body">
 								<div class="table-responsive">
