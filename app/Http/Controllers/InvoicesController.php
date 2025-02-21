41d212557493416469af1872c5bfe9238e4a40de
mariam <mariamemampp00@gmail.com>
2025-02-21 12:42:46 +0200

    insert table invoices & attachments & details

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
+use App\Models\InvoiceAttachment;
+use App\Models\Invoicedetail;
 use App\Models\invoices;
+use App\Models\Section;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class InvoicesController extends Controller
 {
@@ -20,7 +26,8 @@ class InvoicesController extends Controller
      */
     public function create()
     {
-        //
+        $sections = Section::all();
+        return view('invoices.add',compact('sections'));
     }
 
     /**
@@ -28,7 +35,56 @@ class InvoicesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Invoice::create([
+            'invoice_number' => $request->invoice_number,
+            'invoice_Date' => $request->invoice_Date,
+            'Due_date' => $request->Due_date,
+            'product' => $request->product,
+            'section_id' => $request->Section,
+            'Amount_collection' => $request->Amount_collection,
+            'Amount_Commission' => $request->Amount_Commission,
+            'Discount' => $request->Discount,
+            'Value_VAT' => $request->Value_VAT,
+            'Rate_VAT' => $request->Rate_VAT,
+            'Total' => $request->Total,
+            'Status' => 'غير مدفوعة',
+            'Value_Status' => 2,
+            'note' => $request->note,
+        ]);
+        $invoices_id = Invoice::latest()->first()->id;
+        InvoiceDetail::create([
+            'id_Invoice' => $invoices_id,
+            'invoice_number' => $request->invoice_number,
+            'product' => $request->product,
+            'Section' => $request->Section,
+            'Status' => 'غير مدفوعة',
+            'Value_Status' => 2,
+            'note' => $request->note,
+            'user' => auth()->user()->name,
+
+        ]);
+        if ($request->hasFile('pic')) {
+
+            $invoice_id = Invoice::latest()->first()->id;
+            $image = $request->file('pic');
+            $file_name = $image->getClientOriginalName();
+            $invoice_number = $request->invoice_number;
+
+            $attachments = new InvoiceAttachment();
+            $attachments->file_name = $file_name;
+            $attachments->invoice_number = $invoice_number;
+            $attachments->Created_by = Auth::user()->name;
+            $attachments->invoice_id = $invoice_id;
+            $attachments->save();
+
+            // move pic
+            $imageName = $request->pic->getClientOriginalName();
+            $request->pic->move(public_path('Attachments/' . $invoice_number), $imageName);
+
+            
+            session()->flash('Add', 'تم اضافة الفاتورة بنجاح');
+            return back();
+        }
     }
 
     /**
@@ -62,4 +118,10 @@ class InvoicesController extends Controller
     {
         //
     }
+    public function getProducts($id)
+    {
+        $products = DB::table("products")->where("section_id",$id)->pluck("product_name","id");
+        return json_encode($products);
+    }
+        
 }
