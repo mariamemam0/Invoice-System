1551b71c8613b02885885291ba09d2d3ea6d3260
mariam <mariamemampp00@gmail.com>
2025-02-21 12:40:26 +0200

    make route for sava section_id

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::resource('invoices',InvoicesController::class);
 Route::resource('sections',SectionsController::class);
 Route::resource('products',ProductController::class);
+Route::get('/section/{id}',[InvoicesController::class,'getProducts']);
 
 Route::get('/{page}', [AdminController::class,'index']);
 
