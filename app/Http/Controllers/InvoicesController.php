c3a715f5fdb24b8da5f8818f1191ddeb2b95fc48
mariam <mariamemampp00@gmail.com>
2025-02-27 13:51:42 +0200

    pass invoices parameter

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -18,7 +18,8 @@ class InvoicesController extends Controller
      */
     public function index()
     {
-        return view('invoices.invoices');
+        $invoices = Invoice::all();
+        return view('invoices.invoices',compact('invoices'));
     }
 
     /**
