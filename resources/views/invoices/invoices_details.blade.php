730884bf7c176b2fe5da96f14f3a30a438a62ab0
mariam <mariamemampp00@gmail.com>
2025-03-01 17:55:22 +0200

    add view to add new attachments

--- resources/views/invoices/invoices_details.blade.php
+++ resources/views/invoices/invoices_details.blade.php
@@ -202,6 +202,29 @@
                                         <div class="tab-pane" id="tab6">
                                             <!--المرفقات-->
                                             <div class="tab-pane" id="tab6">
+                                            <div class="card card-statistics">
+                                                    <div class="card-body">
+                                                        <p class="text-danger">* صيغة المرفق pdf, jpeg ,.jpg , png </p>
+                                                        <h5 class="card-title">اضافة مرفقات</h5>
+                                                        <form method="post" action="{{ url('/InvoiceAttachments') }}"
+                                                            enctype="multipart/form-data">
+                                                            {{ csrf_field() }}
+                                                            <div class="custom-file">
+                                                                <input type="file" class="custom-file-input" id="customFile"
+                                                                    name="file_name" required>
+                                                                <input type="hidden" id="customFile" name="invoice_number"
+                                                                    value="{{ $invoices->invoice_number }}">
+                                                                <input type="hidden" id="invoice_id" name="invoice_id"
+                                                                    value="{{ $invoices->id }}">
+                                                                <label class="custom-file-label" for="customFile">حدد
+                                                                    المرفق</label>
+                                                            </div><br><br>
+                                                            <button type="submit" class="btn btn-primary btn-sm "
+                                                                name="uploadedFile">تاكيد</button>
+                                                        </form>
+                                                    </div>
+                                                
+                                                <br>
                                             <div class="table-responsive mt-15">
                                                 <table class="table center-aligned-table mb-0 table-hover"
                                                     style="text-align:center">
