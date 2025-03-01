b4911078cd968824cd8250fc54b470066eb6654e
mariam <mariamemampp00@gmail.com>
2025-03-01 17:55:51 +0200

    add new option to invoices

--- resources/views/invoices/invoices.blade.php
+++ resources/views/invoices/invoices.blade.php
@@ -56,6 +56,7 @@
 												<th class="wd-25p border-bottom-0">الإجمالي</th>
 												<th class="wd-25p border-bottom-0">الحالة</th>
 												<th class="wd-25p border-bottom-0">الملاحظات</th>
+												<th class="border-bottom-0">العمليات</th>
 
 
 
@@ -96,6 +97,16 @@
 													@endif
 												</td>
 												<td>{{ $invoice->note}}</td>
+												<td>
+												<div class="dropdown">
+                                                <button aria-expanded="false" aria-haspopup="true"
+                                                    class="btn ripple btn-primary btn-sm" data-toggle="dropdown"
+                                                    type="button">العمليات<i class="fas fa-caret-down ml-1"></i></button>
+                                                <div class="dropdown-menu tx-13">
+												<a class="dropdown-item"
+                                                            href=" {{ url('edit_invoice') }}/{{ $invoice->id }}">تعديل
+                                                            الفاتورة</a>
+												</td>
 
 											</tr>
 											@endforeach
