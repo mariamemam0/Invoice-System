57aa7f76aa651aa1cfe4c7d6debe4a3a50bee545
mariam <mariamemampp00@gmail.com>
2025-03-18 12:36:51 +0200

    make routes to handle the payment method

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,8 @@ Route::post('delete_file',[InvoiceDetailController::class,'destroy'])->name('del
 Route::resource('InvoiceAttachments',InvoiceAttachmentController::class);
 Route::get('/edit_invoice/{id}',[InvoicesController::class,'edit']);
 
-
+Route::get('/Status_show/{id}',[InvoicesController::class,'show'])->name('Status_show');
+Route::post('/Status_Update/{id}',[InvoicesController::class,'Status_Update'])->name('Status_Update');
 Route::get('/{page}', [AdminController::class,'index']);
 
 
